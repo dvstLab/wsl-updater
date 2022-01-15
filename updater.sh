affe322d0082cde1908d79e318fbc81932a707ba
dαvιѕтα <rzlamrr.dvst@protonmail.com>
2022-01-16 05:33:31 +0700

    fix file versioning
    also overwrite wslconfig

--- updater.sh
+++ updater.sh
@@ -3,21 +3,28 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 # Copyright (C) 2021 rzlamrr
 
-PACKAGE_DIR="/mnt/d/Linux" # use /mnt/c instead of C:\
+USER='dvst'
+PACKAGE_DIR="/mnt/d/Linux" # use /mnt/d instead of D:\
 NEW_VERSION=$(curl --silent "https://github.com/nathanchance/WSL2-Linux-Kernel/releases/latest" | sed "s#.*tag/\(.*\)\".*#\1#")
-[[ -f ${PACKAGE_DIR}/version ]] && CURRENT_VERSION=$(cat ${PACKAGE_DIR}/version)
+
+VERFILE="${PACKAGE_DIR}/.version"
+CONFIG="/mnt/c/Users/${USER}/.wslconfig"
+
+[[ -f ${VERFILE} ]] && CURRENT_VERSION=$(cat ${VERFILE}) || CURRENT_VERSION="Unknow Version"
 [[ -f ~/bzImage ]] && rm ~/bzImage
 
 if [[ "${CURRENT_VERSION}" == "" ]] || [[ "${CURRENT_VERSION}" != "${NEW_VERSION}" ]]; then
-    echo "Downloading new update (${NEW_VERSION})!"
-    curl --silent -L -o ~/bzImage "https://github.com/nathanchance/WSL2-Linux-Kernel/releases/download/${NEW_VERSION}/bzImage"
+    echo "Upgrading from ${PACKAGE_DIR} to ${NEW_VERSION}!"
     [[ -f ${PACKAGE_DIR}/bzImage ]] && mv ${PACKAGE_DIR}/bzImage ${PACKAGE_DIR}/bzImage-old
-    mv ~/bzImage ${PACKAGE_DIR}/bzImage
-    echo "${NEW_VERSION}" > ${PACKAGE_DIR}/version
-    echo "The newest version of the WSL kernel has now been installed!"
+    curl --silent -L -o ${PACKAGE_DIR}/bzImage "https://github.com/nathanchance/WSL2-Linux-Kernel/releases/download/${NEW_VERSION}/bzImage"
+    echo "${NEW_VERSION}" > ${VERFILE}
+    echo "${NEW_VERSION} has been installed!"
 else
     echo "Kernel is already at latest version ($CURRENT_VERSION)!"
 fi
+
+echo '[wsl2]' > ${CONFIG}
+echo "kernel = D:\\\\Users\\\\${USER}\\\\Linux\\\\bzImage" >> ${CONFIG}
 
 echo "To load the new kernel, execute \"wsl.exe --shutdown\" within Windows to terminate the WSL VM."
 echo "Afterwards, restart your distro."
